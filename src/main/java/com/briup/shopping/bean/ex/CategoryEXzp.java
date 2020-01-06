268e47e1c1d8e11d361f135c92b61cbddf91b9fd
zhangpan <1295762586@qq.com>
2020-01-06 11:31:42 +0800

    上传

---
+++ src/main/java/com/briup/shopping/bean/ex/CategoryEXzp.java
@@ -0,0 +1,15 @@
+package com.briup.shopping.bean.ex;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class CategoryEXzp implements Serializable {
+    private Integer id;
+
+    private String name;
+
+    private String description;
+
+    List<GoodsEXzp1> goodsEXzp1List;
+
+}
---
+++ src/main/java/com/briup/shopping/mapper/ex/CategoryEXMapper.java
@@ -0,0 +1,9 @@
+package com.briup.shopping.mapper.ex;
+
+import com.briup.shopping.bean.ex.CategoryEXzp;
+
+import java.util.List;
+
+public interface CategoryEXMapper {
+    List<CategoryEXzp> findAllcategory();
+}
